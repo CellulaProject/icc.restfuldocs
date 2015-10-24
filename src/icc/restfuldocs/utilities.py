8a140a20e0a9fc374251769a97ca4387c077e46a
Evgeny Cherkashin <eugeneai@irnok.net>
2015-10-24 18:40:05 +0000

    Kyotocabinet now works.

--- src/icc/restfuldocs/utilities.py
+++ src/icc/restfuldocs/utilities.py
@@ -1,6 +1,6 @@
-#from icc.contentstorage.kyotocabinet.components import *
+from icc.contentstorage.kyotocabinet.components import *
 #from icc.contentstorage.dictionary.components import *
-from icc.contentstorage.kyototycoon.components import *
+#from icc.contentstorage.kyototycoon.components import *
 from icc.restfuldocs.interfaces import IConfiguration
 from zope.component import getUtility
 
@@ -8,6 +8,6 @@ app=getUtility(IConfiguration, 'application')
 
 CONFIG=app.CONFIG
 
-#storage=KiotoCabinetDocStorage(CONFIG['CONTENT_STORAGE_FILENAME'])
-storage=KiotoTycoonDocStorage()
+storage=KiotoCabinetDocStorage(CONFIG['CONTENT_STORAGE_FILENAME'])
+#storage=KiotoTycoonDocStorage()
 #storage=DictionaryDocStorage()

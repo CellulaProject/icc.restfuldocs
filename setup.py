5bdcdca20923fa355f0583c59e15a6f1b0e6b7a8
Evgeny Cherkashin <eugeneai@irnok.net>
2015-10-24 12:31:43 +0000

    Tried to run, but failed.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
 	'pyramid==1.6a2',
 	'waitress==0.8.11dev0ipv6-1',
-	'cornice',
+	'cornice==1.2.0.dev0',
         #'flask',
         #'flask-httpauth',
         #'flask-restful',
@@ -23,6 +23,7 @@ setup(
     dependency_links = [
         'https://github.com/Pylons/pyramid/archive/1.6a2.zip#egg=pyramid-1.6a2',
         'https://github.com/eugeneai/waitress/archive/0.8.11dev0ipv6-1.zip#egg=waitress-0.8.11dev0ipv6-1',
+        'https://github.com/mozilla-services/cornice/archive/master.zip#egg=cornice-1.2.0.dev0'
 	],
     entry_points="""\
     [paste.app_factory]
--- src/icc/restfuldocs/views.py
+++ src/icc/restfuldocs/views.py
@@ -2,10 +2,9 @@ from icc.restfuldocs.interfaces import IConfiguration
 from icc.contentstorage.interfaces import IDocumentStorage
 from zope.interface import implementer
 from zope.component import getGlobalSiteManager, getUtility
-from cornice import Service
+from cornice.resource import add_resource, add_view
+from pyramid.response import Response
 
-doc_list = Service(name='list', path='/content', description="List of documents API")
-doc = Service(name='content', path='/content/{sha_id}', description="Document API")
 
 def storage(name='documents'):
     """Returns thw pointer to the document storage
@@ -18,11 +17,8 @@ def app(name='application'):
     """
     return getUtility(IApplication, name)
 
-@doc_list.get()
-def get_info(request):
-    """Returns Hello in JSON."""
-    return {'Hello': 'World'}
-
+def abort(status):
+    return Response(status_code=status)
 
 # @auth.get_password
 # def get_password(username):
@@ -37,28 +33,37 @@ def get_info(request):
 #     # auth dialog
 #     return make_response(jsonify({'message': 'Unauthorized access'}), 401)
 
-@doc_list.post()
-def post_doc():
-    """Append new document to the storage"""
 
-    data=request.get_data()
-    sha_id=storage().put(data)
-    return {'id': sha_id}, 201
+class Documents(object):
+    """
+    """
 
-@doc.get()
-def get(sha_id):
+    def __init__(self, request):
+        """
+        """
+        self.request=request
+
+    def post_doc():
+        """Append new document to the storage"""
+
+        data=request.get_data()
+        sha_id=storage().put(data)
+        return {'id': sha_id}, 201
+
+    def get(sha_id):
         if len(sha_id) != 64:
             abort(404)
         try:
             data=storage().get(sha_id)
         except KeyError as e:
             return {'id': sha_id, 'exception':'KeyError', 'value': str(e)}
-        response=make_response(data)
-        response.headers['Content-Type'] = 'Content-Type:application/octet-stream'
+        response=Response(
+            content_type='application/octet-stream',
+            body=data
+        )
         return response
 
-@doc.delete()
-def delete(self, sha_id):
+    def delete(self, sha_id):
         if len(sha_id) != 64:
             abort(404)
         try:
@@ -67,9 +72,14 @@ def delete(self, sha_id):
             return {'id': sha_id, 'exception':'KeyError', 'value': str(e)}
         return {'id': key, 'deleted':True}
 
+add_view(Documents.post_doc, renderer='json')
+add_view(Documents.get, renderer='json')
+add_view(Documents.delete, renderer='json')
+
+document_resource = add_resource(
+    Documents,
+    path='/content/{sha_id}'
+) #, description="Document API")
+
 def includeme(config):
-    'Add routes'
-    # config.scan(__name__)
-    # config.add_route('index', '')
-    # config.add_route('debug', 'debug')
-    # config.add_route('pdb', 'pdb')
+    config.add_resource(document_resource)
